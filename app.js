ca455dbe8fcff1ebd8c6b370ce089827568af236
Florian BORIE <florian.borie@ynov.com>
2020-01-13 21:34:24 +0100

    add file with better structure ans requests in promise

--- app.js
+++ app.js
@@ -1,190 +1,71 @@
 require('babel-register')
-const {success, error} = require('functions')
-const mysql = require('mysql')
+const {success, error, checkAndChange} = require('./assets/functions')
+const mysql = require('promise-mysql')
 const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
-const morgan = require('morgan')
-const config = require('./config')
+const morgan = require('morgan')('dev')
+const config = require('./assets/config')
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    database: 'backendproject',
-    user: 'root',
-    password: ''
-})
-
-db.connect((err) => {
-    if(err){
-        console.log(err.message)
-    } else {
-
-        console.log('Connected.')
+    host: config.db.host,
+    database: config.db.database,
+    user: config.db.user,
+    password: config.db.password
+}).then((db) => {
+    console.log('Connected.')
 
         const app = express()
 
         let MembersRouter = express.Router()
+        let Members = require('./assets/classes/members-class')(db, config)
 
-        app.use(morgan('dev'))
+        app.use(morgan)
         app.use(bodyParser.json());
         app.use(bodyParser.urlencoded({ extended : true }));
 
         MembersRouter.route('/:id')
 
             // Récupère un membre avec son ID
-            .get((req, res) => {
-
-                db.query('SELECT * FROM members WHERE id = ?', [req.params.id], (err, result) => {
-                    if(err) {
-                        res.json(error(err.message))
-                    }else{
-
-                        if(result[0] != undefined){
-                            res.json(success(result[0]))
-                        } else {
-                            res.json(error('Wrong id'))
-                        }
-                        
-                    }
-                })
+            .get(async (req, res) => {
+                let member = await Members.getById(req.params.id)
+                res.json(checkAndChange(member))
             })
 
             // Modifie un membre avec son ID
-            .put((req, res) => {
+            .put(async (req, res) => {
 
-                if(req.body.mail) {
-
-                    db.query('SELECT * FROM members WHERE id = ?', [req.params.id], (err, result) => {
-                        if(err) {
-                            res.json(error(err.message))
-                        }else{
-    
-                            if(result[0] != undefined){
-                                
-                                db.query('SELECT * FROM members WHERE mail = ? AND id != ?', [req.body.mail,req.params.id], (err, result) => {
-                                    if(err) {
-                                        res.json(error(err.message))
-                                    } else {
-                                        if(result[0] != undefined) {
-                                            res.json(error('same mail'))
-                                        } else {
-
-                                            db.query('UPDATE members SET firstname = ?, lastname = ?, age = ?, mail = ? where id = ?', [req.body.firstname, req.body.lastname, req.body.age, req.body.mail, req.params.id], (err, result) => {
-                                                if(err) {
-                                                    res.json(error(err.message))
-                                                } else {
-                                                    res.json(success(true))
-                                                }
-                                            })
-                                        }
-                                    }
-                                })
-                            } else {
-                                res.json(error('Wrong id'))
-                            }
-                        }
-                    })
-                } else {
-                    res.json(error('no name value'))
-                }
+                let updateMember = await Members.update(req.params.id, req.body.firstname, req.body.lastname, req.body.age, req.body.mail)
+                res.json(checkAndChange(updateMember))
+                
             })
 
             // Suprime un membre avec son ID
-            .delete ((req, res) => {
+            .delete (async (req, res) => {
 
-                let index = getIndex(req.params.id);
-
-                if (typeof(index) == 'string') {
-                    res.json(error(index))
-                } else {
-
-                    members.splice(index, 1)
-                    res.json(success(members))
-
-                }
+                let deleteMember = await Members.delete(req.params.id)
+                res.json(checkAndChange(deleteMember))
             })
             
         MembersRouter.route('/')
 
             // Récupère tous les membres
-            .get((req, res) => {
-                if(req.query.max != undefined && req.query.max > 0) {
-
-                    db.query('SELECT * FROM members LIMIT 0, ?', [req.query.max], (err, result) => {
-                        if(err) {
-                            res.json(error(err.message))
-                        } else {
-                            res.json(success(result))
-                        }
-                            
-                    })
-                }else if(req.query.max != undefined){
-                    res.json(error('Wrong max value'))
-                }else{
-                    db.query('SELECT * FROM members', (err, result) => {
-                        if(err) {
-                            res.json(error(err.message))
-                        } else {
-                            res.json(success(result))
-                        }
-                            
-                    })
-                    res.json(success(members[0].firstname)) 
-                }
+            .get(async (req, res) => {
+                let allMembers = await Members.getAll(req.query.max)
+                res.json(checkAndChange(allMembers))
+                
             })
 
             // Ajoute un membre
-            .post((req, res) => {
-                if(req.body.mail) {
-
-                    db.query('SELECT * FROM members WHERE mail = ?', [req.body.mail], (err, result) => {
-                        if(err) {
-                            res.json(error(err.message))
-                        } else {
-                            if (result[0] != undefined) {
-                                res.json(error('Wrong mail'))
-                            } else {
-                                db.query('INSERT INTO members(firstname, lastname, age, mail) VALUES(?, ?, ?, ?)', [req.body.firstname, req.body.lastname, req.body.age, req.body.mail], (err, result) => {
-                                    if(err) {
-                                        res.json(error(err.message))
-                                    } else {
-                                        db.query('SELECT * FROM members WHERE mail = ?', [req.body.mail], (err, result) => {
-                                            if(err) {
-                                                res.json(error(err.message))
-                                            } else {
-                                                res.json(success({
-                                                    id: result[0].id,
-                                                    firstname: result[0].firstname,
-                                                    lastname: result[0].lastname,
-                                                    age: result[0].age,
-                                                    mail: result[0].mail
-                                                }))
-                                            }
-                                        })
-                                    }
-                                })
-                            }
-                        }
-                    })
-                }else{
-                    res.json(error('no name value'))
-                }
+            .post(async (req, res) => {
+                let addMember = await Members.add(req.body.firstname, req.body.lastname, req.body.age, req.body.mail)
+                res.json(checkAndChange(addMember))
             })
 
         app.use(config.rootAPI+'members', MembersRouter)
 
         app.listen(config.port, () => console.log('Started on port '+config.port))
-    }
+}).catch((err) => {
+    console.log('Error during database connection')
+    console.log(err.message)
 })
-
-function getIndex(id) {
-    for (let i = 0; i < members.length; i++) {
-        if(members[i].id == id)
-            return i
-    }
-    return 'wrong id'
-}
-
-function createID() {
-    return lastMember = members[members.length-1].id + 1
-}
---
+++ assets/classes/members-class.js
@@ -0,0 +1,130 @@
+let db, confnig
+
+module.exports = (_db, _config) => {
+    db = _db
+    config = _config
+    return Members
+}
+
+let Members = class {
+
+    static getById(id) {
+
+        return new Promise((next) => {
+            db.query('SELECT * FROM members WHERE id = ?', [id])
+                .then((result) => {
+                    if(result[0] != undefined){
+                        next(result[0])
+                    } else {
+                        next(new Error(config.errors.wrongID))
+                    }
+                })
+                .catch((err) => next(err))
+        })
+
+    }
+
+    static getAll(max) {
+
+        return new Promise((next) => {
+
+            if(max != undefined && max > 0) {
+
+            db.query('SELECT * FROM members LIMIT 0, ?', [parseInt(max)])
+                .then((result) => next(result))
+                .catch((err) => next(err))
+            }else if(max != undefined){
+                next(new Error(config.errors.wrongMaxValue))
+            }else{
+
+                db.query('SELECT * FROM members')
+                    .then((result) => next(result))
+                    .catch((err) => next(err))
+            }
+
+        })
+    }
+
+    static add(firstname, lastname, age, mail) {
+
+        return new Promise((next) => {
+
+            if(mail != undefined && mail.trim() != '') {
+
+                mail = mail.trim()
+
+                db.query('SELECT * FROM members WHERE mail = ?', [mail])
+                    .then((result) => {
+                        if (result[0] != undefined) {
+                            next(new Error(config.errors.wrongMail))
+                        } else {
+                            return db.query('INSERT INTO members(firstname, lastname, age, mail) VALUES(?, ?, ?, ?)', [firstname, lastname, age, mail])
+                        }
+                    })
+                    .then(() => {
+                        return db.query('SELECT * FROM members WHERE mail = ?', [mail])
+                    })
+                    .then((result) => {
+                        next({
+                            id: result[0].id,
+                            firstname: result[0].firstname,
+                            lastname: result[0].lastname,
+                            age: result[0].age,
+                            mail: result[0].mail
+                        })                       
+                    })
+                    .catch((err) => next(err))
+            }else{
+                next(new Error(config.errors.noNameValue))
+            }
+        })
+
+    }
+
+    static update(id, firstname, lastname, age, mail) {
+
+        return new Promise((next) => {
+
+            if(mail != undefined && mail.trim() != '') {
+
+                mail = mail.trim()
+
+                db.query('SELECT * FROM members WHERE id = ?', [id])
+                    .then((result) => {
+                        if(result[0] != undefined){                            
+                            return db.query('SELECT * FROM members WHERE mail = ? AND id != ?', [mail,id])
+                        } else {
+                            next(new Error(config.errors.wrongID))
+                        }
+                    })
+                    .then((result) => {
+                        if(result[0] != undefined) {
+                            next(new Error(config.errors.sameName))
+                        } else {
+                            return db.query('UPDATE members SET firstname = ?, lastname = ?, age = ?, mail = ? where id = ?', [firstname, lastname, age, mail, id])
+                        }  
+                    })
+                    .then(() => next(true))
+                    .catch((err) => next(err))
+            } else {
+                next(new Error(config.errors.noNameValue))
+            }
+        })   
+    }
+
+    static delete(id){
+        return new Promise((next) => {
+            db.query('SELECT * FROM members WHERE id = ?', [id])
+                .then((result) => {
+                    if(result[0] != undefined){
+                        return db.query('DELETE FROM members Where id = ?', [id])
+                    } else {
+                    next(new Error(config.errors.wrongID))
+                    } 
+                })
+                .then(() => next(true))
+                .catch((err) => next(err))
+        })
+    }
+
+}
