633034c77fbfd007a4625795e36180affbe6242a
TheoCharron <theocharron@protonmail.com>
2020-02-18 09:23:46 +0100

    update authentification

--- assets/authentification.js
+++ assets/authentification.js
@@ -135,3 +135,24 @@ module.exports = function(passport) {
     )
   );
 };
+
+// JWT part
+
+const JWTstrategy = require("passport-jwt").Strategy;
+const ExtractJWT = require("passport-jwt").ExtractJwt;
+
+passport.use(
+  new JWTstrategy(
+    {
+      secretOrKey: "top_secret",
+      jwtFromRequest: ExtractJWT.fromUrlQueryParameter("secret_token")
+    },
+    async (token, done) => {
+      try {
+        return done(null, token.user);
+      } catch (error) {
+        done(error);
+      }
+    }
+  )
+);
